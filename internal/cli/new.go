ee2304fb720757e57b65fd3fa486bea6aa1182fe
JuD4Mo <juan_d.munoz_o@uao.edu.co>
2026-05-07 21:18:01 -0500

    Refactor project creation command to validate input and enhance error handling

--- internal/cli/new.go
+++ internal/cli/new.go
@@ -3,25 +3,33 @@ package cli
 import (
 	"fmt"
 
+	"github.com/Jud4Mo/multi-templates/internal/catalog"
+	"github.com/Jud4Mo/multi-templates/internal/prompt"
 	"github.com/spf13/cobra"
 )
 
 var (
-	newLang      string
-	newFramework string
-	newArch      string
-	newName      string
+	newLang        string
+	newFramework   string
+	newArch        string
+	newProjectType string
+	newName        string
 )
 
 var newCmd = &cobra.Command{
 	Use:   "new",
 	Short: "Generate a project from a template",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("New Project:")
-		fmt.Println(" lang:      ", newLang)
-		fmt.Println(" framework: ", newFramework)
-		fmt.Println(" arch:      ", newArch)
-		fmt.Println(" name:      ", newName)
+	RunE: func(cmd *cobra.Command, args []string) error {
+		input := prompt.NewProjectFromInputFlags(newLang, newFramework, newProjectType, newArch, newName)
+		cat := catalog.NewCatalog()
+
+		err := validateProjectInput(cat, input)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("project configuration is valid")
+		return nil
 	},
 }
 
@@ -29,8 +37,34 @@ var newCmd = &cobra.Command{
 func init() {
 	newCmd.Flags().StringVar(&newLang, "lang", "", "Language (go, java, ts...)")
 	newCmd.Flags().StringVar(&newFramework, "framework", "", "Framework (gin, echo, spring, express...)")
-	newCmd.Flags().StringVar(&newArch, "arch", "", "Architecture (layered, hexagonal, clean...)")
+	newCmd.Flags().StringVar(&newArch, "arch", "", "Code level architecture (layered, hexagonal, clean...)")
+	newCmd.Flags().StringVar(&newProjectType, "type", "", "System level architecture (monolith, microservices...)")
 	newCmd.Flags().StringVar(&newName, "name", "", "Name of the project to be created")
 
 	rootCmd.AddCommand(newCmd)
+}
+
+func validateProjectInput(cat catalog.Catalog, input prompt.NewProjectInput) error {
+	if input.Name == "" {
+		return fmt.Errorf("project name required")
+	}
+
+	stack := catalog.Stack{
+		Language:     input.Language,
+		Framework:    input.Framework,
+		ProjectType:  input.ProjectType,
+		Architecture: input.Architecture,
+	}
+
+	if !cat.IsSupported(stack) {
+		return fmt.Errorf("unsupported stack")
+	}
+
+	fmt.Println("project configuration is valid")
+	fmt.Println(" language:     ", input.Language)
+	fmt.Println(" framework:    ", input.Framework)
+	fmt.Println(" type:         ", input.ProjectType)
+	fmt.Println(" architecture: ", input.Architecture)
+	fmt.Println(" name:         ", input.Name)
+	return nil
 }
--- internal/prompt/input.go
+++ internal/prompt/input.go
@@ -9,3 +9,13 @@ type NewProjectInput struct {
 	Architecture catalog.Architecture
 	Name         string
 }
+
+func NewProjectFromInputFlags(lang string, framework string, projectType string, arch string, name string) NewProjectInput {
+	return NewProjectInput{
+		Language:     catalog.Language(lang),
+		Framework:    catalog.Framework(framework),
+		ProjectType:  catalog.ProjectType(projectType),
+		Architecture: catalog.Architecture(arch),
+		Name:         name,
+	}
+}
